9366edf1c062fc135d2288cccb6152c2c7437c6a
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-10-18 23:40:10 -0700

    added new syntax examples for checkboxes, radio selections

--- example.php
+++ example.php
@@ -1,10 +1,9 @@
 <?php
 /*
 
-	Example shows a custom class, and use of load and make.
+	Example shows a custom class, use of load and make, coded string syntax for
+	checkbox, radio and select options (which require assoc. arrays).
 	
-	To Do : Provide an additional class for poform classes to inherit that 
-	will influence the way the forms fields appear... 
 */
 require('poform.php');
 
@@ -17,8 +16,16 @@ class form_class{
 	// a-  b- etc refers to the value, while the text after it and before the ':' defines
 	// what appears in the selection list, or use helper function to convert assoc. arrays
 	public $selection = 'select a-Value A:b-value B:c-value C';
+	
+	public $a = array('select'=> array('1'=>'Value 1','2'=>'Value 2','3'=>'value 3'));
+	
+	public $checkbox = array('checkbox:fav_number'=> array('1'=>'Value 1','2'=>'Value 2','3'=>'value 3'));
+		
+	public $radio = array('radio:preference'=> array('y'=>'No','n'=>'Yes','m'=>'Maybe'));
 
 }
+
+// or create custom coded arrays that specify the action of the array (checkbox/radio/select)
 
 $a = new form_class();
 
@@ -28,4 +35,4 @@ $b = poform::load($a);
 
 print_r($b);
 
-poform::make($b);
+echo poform::make($b);
